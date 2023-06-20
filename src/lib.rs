bb6959a00d1389d6f10e62f42392b988ab7bad9a
OverzealousLotus <aminaabdul.9kg55@slmail.me>
2023-06-19 23:25:51 -0400

    Add `VecDeque`, and `LinkedList` macro.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 //! # Macroland
 //!
 //! Macroland is a Rust Crate, which provides macros for shorthand definitions
-//! of various types.
+//! of various types. Think how `vec!` is used to create empty, or filled up vectors.
+//! Macroland is full of various macros for this exact purpose.
 
-/// `binary_heap` creates a Binary Heap. This can be done in two different ways.
+/// `binary_heap` creates a Binary Heap. You can either create an empty variant, with
+/// its type explicity given, or a list of values.
 ///
 /// # Examples
 ///
@@ -15,7 +17,7 @@
 /// let mut uninit_binary_heap = binary_heap!(usize);
 /// uninit_binary_heap.push(100);
 /// // Or
-/// let my_binary_heap: BinaryHeap<usize> = binary_heap!(100);
+/// let my_binary_heap: BinaryHeap<usize> = binary_heap!(100, 200, 300);
 ///
 /// assert_eq!(uninit_binary_heap, my_binary_heap);
 /// ```
@@ -23,6 +25,8 @@
 macro_rules! binary_heap {
     ($value:ty) => {
         {
+            use std::collections::BinaryHeap;
+
             let new_binary_heap: BinaryHeap<$value> = BinaryHeap::new();
             new_binary_heap
         }
@@ -30,18 +34,24 @@ macro_rules! binary_heap {
 
     ( $($value:expr),* ) => {
         {
+            use std::collections::BinaryHeap;
+
             let mut new_binary_heap = BinaryHeap::new();
             $( new_binary_heap.push($value); )*
+
             new_binary_heap
         }
     };
 }
 
-/// `btreeset!` creates a BTreeSet. This can be done in two different ways.
+/// `btreeset!` creates a BTreeSet. Like HashSet, you can predefine its members, or the
+/// type of its future members.
 ///
 /// # Examples
 ///
 /// ```
+/// use std::collections::BTreeSet;
+///
 /// use macroland::btreeset;
 ///
 /// let mut uninit_btreeset = btreeset!(&str);
@@ -55,6 +65,8 @@ macro_rules! binary_heap {
 macro_rules! btreeset {
     ($value:ty) => {
         {
+            use std::collections::BTreeSet;
+
             let new_btreeset: BTreeSet<$value> = BTreeSet::new();
             new_btreeset
         }
@@ -62,8 +74,11 @@ macro_rules! btreeset {
 
     ( $($value:expr),*) => {
         {
+            use std::collections::BTreeSet;
+
             let mut new_btreeset = BTreeSet::new();
             $( new_btreeset.insert($value); )*
+
             new_btreeset
         }
     };
@@ -74,6 +89,8 @@ macro_rules! btreeset {
 /// # Examples
 ///
 /// ```
+/// use std::collections::BTreeMap
+///
 /// use macroland::btreemap;
 ///
 /// let mut uninit_btreemap = btreemap!(&str, usize);
@@ -93,6 +110,8 @@ macro_rules! btreeset {
 macro_rules! btreemap {
     ($key:ty, $value:ty) => {
         {
+            use std::collections::BTreeMap;
+
             let new_btreemap: BTreeMap<$key, $value> = BTreeMap::new();
             new_btreemap
         }
@@ -100,18 +119,44 @@ macro_rules! btreemap {
 
     ($($key:expr => $value:expr),*) => {
         {
+            use std::collections::BTreeMap;
+
             let mut new_btreemap = BTreeMap::new();
             $( new_btreemap.insert($key, $value); )*
+
             new_btreemap
         }
     }
 }
 
-/// `hashset!` creates a HashSet. This can be done in two different ways.
+/// `boxed!` provides a simple way to box a given type, without using ::new() each
+/// time you want to.
+///
+/// # Example
+///
+/// ```
+/// use macroland::boxed;
+///
+/// let macroland_box = boxed!(Some(100));
+/// let normal_box = Box::new(Some(100));
+///
+/// assert_eq!(macroland_box, normal_box);
+/// ```
+#[macro_export]
+macro_rules! boxed {
+    ($value:expr) => {{
+        Box::new($value)
+    }};
+}
+
+/// `hashset!` creates a HashSet. You can either define its members during the macro call,
+/// or create an empty HashSet with its type predefined.
 ///
 /// # Examples
 ///
 /// ```
+/// use std::collections::HashSet;
+///
 /// use macroland::hashset;
 ///
 /// let mut uninit_hashset = hashset!(&str);
@@ -125,24 +170,33 @@ macro_rules! btreemap {
 macro_rules! hashset {
     ($value:ty) => {
         {
+            use std::collections::HashSet;
+
             let new_hashset: HashSet<$value> = HashSet::new();
             new_hashset
         }
     };
     ($($value:expr),*) => {
         {
+            use std::collections::HashSet;
+
             let mut new_hashset = HashSet::new();
             $( new_hashset.insert($value); )*
+
             new_hashset
         }
     };
 }
 
-/// `hashmap!` creates a HashMap. This can be done in two different ways.
+/// `hashmap!` creates a HashMap. The OG is back, y'all... I'm kidding. I heard about there
+/// being a builtin `hashmap!` macro not too long ago, I guess I wasn't around then. So,
+/// this one is for all those who remember.
 ///
 /// # Examples
 ///
 /// ```
+/// use std::collections::Hashmap;
+///
 /// use macroland::hashmap;
 ///
 /// let mut uninit_hashmap = hashmap!(&str, usize);
@@ -162,34 +216,102 @@ macro_rules! hashset {
 macro_rules! hashmap {
     ($key:ty, $value:ty) => {
         {
+            use std::collections::HashMap;
+
             let new_map: HashMap<$key, $value> = HashMap::new();
             new_map
         }
     };
     ($($key:expr => $value:expr),*) => {
         {
+            use std::collections::HashMap;
+
             let mut new_map = HashMap::new();
             $( new_map.insert($key, $value); )*
+
             new_map
         }
     };
 }
 
-/// `boxed!` provides a simple way to box a given type.
+/// `linkedlist!` creates a LinkedList. Alike to the `vecdeque!` macro, all members are pushed
+/// to the front of the list, when predefined.
 ///
-/// # Example
+/// # Examples
 ///
 /// ```
-/// use macroland::boxed;
+/// use std::collections::LinkedList;
 ///
-/// let macroland_box = boxed!(Some(100));
-/// let normal_box = Box::new(Some(100));
+/// use macroland::linkedlist;
 ///
-/// assert_eq!(macroland_box, normal_box);
+/// let mut uninit_linkedlist = linkedlist!(isize);
+/// uninit_linkedlist.push_front(10);
+/// uninit_linkedlist.push_back(20);
+/// // Or
+/// let my_linkedlist = linkedlist!(
+///     10,
+///     20,
+///     30,
+/// );
 /// ```
 #[macro_export]
-macro_rules! boxed {
-    ($value:expr) => {{
-        Box::new($value)
+macro_rules! linkedlist {
+    ($value:ty) => {
+        {
+            use std::collections::LinkedList;
+
+            let new_linkedlist: LinkedList<$value> = LinkedList::new();
+            new_linkedlist
+        }
+    };
+    ( $($value:expr),* ) => {
+        {
+            use std::collections::LinkedList;
+
+            let mut new_linkedlist = LinkedList::new();
+            $( new_linkedlist.push_front($value); )*
+
+            new_linkedlist
+        }
+    };
+}
+
+/// `vecdeque!` creates a VecDeque. Be warned, members are pushed into the *front* of the
+/// VecDeque<T> when predefining the members using this macro.
+///
+/// # Examples
+///
+/// ```
+/// use std::collections::VecDeque;
+///
+/// use macroland::vecdeque;
+///
+/// let mut uninit_vecdeque = vecdeque!(&str);
+/// uninit_vecdeque.push_front("I'm going ahead!");
+/// uninit_vecdeque.push_back("I'm going behind!");
+/// // Or
+/// let my_vecdeque = vecdeque!(
+///     "I'm always going ahead!",
+///     "No matter how many members are pushed!",
+///     "Uwu",
+/// );
+/// ```
+#[macro_export]
+macro_rules! vecdeque {
+    ($value:ty) => {{
+        use std::collections::VecDeque;
+
+        let new_vecdeque: VecDeque<$value> = VecDeque::new();
+        new_vecdeque
     }};
+    ( $($value:expr),* ) => {
+        {
+            use std::collections::VecDeque;
+
+            let mut new_vecdeque = VecDeque::new();
+            $(new_vecdeque.push_front($value); )*
+
+            new_vecdeque
+        }
+    };
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,6 @@
-use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet};
-
 use macroland::*;
 
 fn main() {
-    let hashmap_mac = hashmap!("uwu" => 100);
-    let mut uninit_hashmap = hashmap!(usize, String);
-
-    let hashset_mac = hashset!(200, 300, 400);
-    let mut uninit_hashset = hashset!(usize);
-
     let btreemap_mac = btreemap!("Hi Ikarite" => true);
     let mut uninit_btreemap = btreemap!(&str, bool);
 
@@ -18,35 +10,61 @@ fn main() {
     let binary_heap_mac = binary_heap!(100);
     let mut uninit_binary_heap = binary_heap!(usize);
 
-    uninit_hashmap.insert(10, String::from("Hi"));
-    uninit_hashset.insert(10);
+    let hashmap_mac = hashmap!("uwu" => 100);
+    let mut uninit_hashmap = hashmap!(usize, String);
+
+    let hashset_mac = hashset!(200, 300, 400);
+    let mut uninit_hashset = hashset!(usize);
+
+    let linkedlist_mac = linkedlist!("UWU", "OWO", "U3U");
+    let mut uninit_linkedlist = linkedlist!(String);
+
+    let vecdeque_mac = vecdeque!("uwu", "owo", "uwe");
+    let mut uninit_vecdeque = vecdeque!(&str);
 
     uninit_btreemap.insert("Ikarite sussy", false);
     uninit_btreeset.insert(1000);
 
     uninit_binary_heap.push(10);
 
+    uninit_hashmap.insert(10, String::from("Hi"));
+    uninit_hashset.insert(10);
+
+    uninit_linkedlist.push_front(String::from("OWE"));
+    uninit_linkedlist.push_back(String::from("YOYOYO"));
+
+    uninit_vecdeque.push_front("Bruh.");
+    uninit_vecdeque.push_back("BABABOEEY");
+
     let boxy = boxed!(10);
 
     println!(
-        "HashMap Macro: {:?} | Uninitialized HashMap: {:?}",
-        hashmap_mac, uninit_hashmap
-    );
-    println!(
-        "HashSet Macro: {:?} | Uninitialized HashSet: {:?}",
-        hashset_mac, uninit_hashset
-    );
-    println!(
-        "BTreeMap Macro: {:?} | Uninitialized BTreeMap: {:?}",
+        "BTreeMap Macro: {:?} | Uninitialized BTreeMap: {:?}\n",
         btreemap_mac, uninit_btreemap
     );
     println!(
-        "BTreeSet Macro: {:?} | Uninitialized BTreeSet: {:?}",
+        "BTreeSet Macro: {:?} | Uninitialized BTreeSet: {:?}\n",
         btreeset_mac, uninit_btreeset
     );
     println!(
-        "BinaryHeap Macro: {:?} | Uninitialized BinaryHeap: {:?}",
+        "BinaryHeap Macro: {:?} | Uninitialized BinaryHeap: {:?}\n",
         binary_heap_mac, uninit_binary_heap
+    );
+    println!(
+        "HashMap Macro: {:?} | Uninitialized HashMap: {:?}\n",
+        hashmap_mac, uninit_hashmap
+    );
+    println!(
+        "HashSet Macro: {:?} | Uninitialized HashSet: {:?}\n",
+        hashset_mac, uninit_hashset
+    );
+    println!(
+        "LinkedList Macro: {:?} | Uninitialized LinkedList: {:?}\n",
+        linkedlist_mac, uninit_linkedlist
+    );
+    println!(
+        "VecDeque Macro: {:?} | Uninitialized VecDeque: {:?}\n",
+        vecdeque_mac, uninit_vecdeque
     );
     println!("Box Macro: {:?}", boxy);
 }
