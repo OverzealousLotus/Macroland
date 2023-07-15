7f9863a7c4e9d65c610d53306c5e1021cb686ac2
OverzealousLotus <aminaabdul.9kg55@slmail.me>
2023-07-15 16:48:15 -0400

    Tidy-up main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,14 @@
 use macroland::*;
 
-fn main() {
+fn btree_macros() {
     let btreemap_mac = btreemap!("Hi Ikarite" => true);
     let mut uninit_btreemap = btreemap!(&str, bool);
 
     let btreeset_mac = btreeset!("Bosnia");
     let mut uninit_btreeset = btreeset!(usize);
 
-    let binary_heap_mac = binary_heap!(100);
-    let mut uninit_binary_heap = binary_heap!(usize);
-
-    let hashmap_mac = hashmap!("uwu" => 100);
-    let mut uninit_hashmap = hashmap!(usize, String);
-
-    let hashset_mac = hashset!(200, 300, 400);
-    let mut uninit_hashset = hashset!(usize);
-
-    let linkedlist_mac = linkedlist!("UWU", "OWO", "U3U");
-    let mut uninit_linkedlist = linkedlist!(String);
-
-    let vecdeque_mac = vecdeque!("uwu", "owo", "uwe");
-    let mut uninit_vecdeque = vecdeque!(&str);
-
     uninit_btreemap.insert("Ikarite sussy", false);
     uninit_btreeset.insert(1000);
-
-    uninit_binary_heap.push(10);
-
-    uninit_hashmap.insert(10, String::from("Hi"));
-    uninit_hashset.insert(10);
-
-    uninit_linkedlist.push_front(String::from("OWE"));
-    uninit_linkedlist.push_back(String::from("YOYOYO"));
-
-    uninit_vecdeque.push_front("Bruh.");
-    uninit_vecdeque.push_back("BABABOEEY");
-
-    let boxy = boxed!(10);
 
     println!(
         "BTreeMap Macro: {:?} | Uninitialized BTreeMap: {:?}\n",
@@ -46,10 +18,35 @@ fn main() {
         "BTreeSet Macro: {:?} | Uninitialized BTreeSet: {:?}\n",
         btreeset_mac, uninit_btreeset
     );
+}
+
+fn cell_macros() {
+    let cell_mac = cell!([1, 2, 3]);
+
+    let refcell_mac = refcell!([4, 5, 6]);
+
+    let uninit_oncecell = oncecell!(String);
+
+    uninit_oncecell.set(String::from("Meow!")).expect("Error");
+
+    println!("Cell Macro: {:?}\n", cell_mac.get());
+    println!("RefCell Macro: {:?}\n", refcell_mac.borrow());
     println!(
-        "BinaryHeap Macro: {:?} | Uninitialized BinaryHeap: {:?}\n",
-        binary_heap_mac, uninit_binary_heap
+        "Uninitialized OnceCell: {:?}\n",
+        uninit_oncecell.get().unwrap()
     );
+}
+
+fn hash_macros() {
+    let hashmap_mac = hashmap!("uwu" => 100);
+    let mut uninit_hashmap = hashmap!(usize, String);
+
+    let hashset_mac = hashset!(200, 300, 400);
+    let mut uninit_hashset = hashset!(usize);
+
+    uninit_hashmap.insert(10, String::from("Hi"));
+    uninit_hashset.insert(10);
+
     println!(
         "HashMap Macro: {:?} | Uninitialized HashMap: {:?}\n",
         hashmap_mac, uninit_hashmap
@@ -57,6 +54,36 @@ fn main() {
     println!(
         "HashSet Macro: {:?} | Uninitialized HashSet: {:?}\n",
         hashset_mac, uninit_hashset
+    );
+}
+
+fn misc_macros() {
+    let boxy = boxed!(10);
+
+    println!("Box Macro: {:?}\n", boxy);
+}
+
+fn other_collection_macros() {
+    let binary_heap_mac = binary_heap!(100);
+    let mut uninit_binary_heap = binary_heap!(usize);
+
+    let linkedlist_mac = linkedlist!("UWU", "OWO", "U3U");
+    let mut uninit_linkedlist = linkedlist!(String);
+
+    let vecdeque_mac = vecdeque!("uwu", "owo", "uwe");
+    let mut uninit_vecdeque = vecdeque!(&str);
+
+    uninit_binary_heap.push(10);
+
+    uninit_linkedlist.push_front(String::from("OWE"));
+    uninit_linkedlist.push_back(String::from("YOYOYO"));
+
+    uninit_vecdeque.push_front("Bruh.");
+    uninit_vecdeque.push_back("BABABOEEY");
+
+    println!(
+        "BinaryHeap Macro: {:?} | Uninitialized BinaryHeap: {:?}\n",
+        binary_heap_mac, uninit_binary_heap
     );
     println!(
         "LinkedList Macro: {:?} | Uninitialized LinkedList: {:?}\n",
@@ -66,5 +93,18 @@ fn main() {
         "VecDeque Macro: {:?} | Uninitialized VecDeque: {:?}\n",
         vecdeque_mac, uninit_vecdeque
     );
-    println!("Box Macro: {:?}", boxy);
+}
+
+fn reference_macros() {
+    let rc_mac = rc!("Mew?");
+
+    println!("Rc Macro: {:?}", rc_mac);
+}
+fn main() {
+    btree_macros();
+    cell_macros();
+    hash_macros();
+    misc_macros();
+    other_collection_macros();
+    reference_macros();
 }
